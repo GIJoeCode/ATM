a1a33a11424ef706dc0ed23dfda353b2a654d7c8
WaxOnWaxOff221 <josephwquinn@gmail.com>
2022-05-10 19:09:39 -0500

    Initial commit - ATM project added

---
+++ src/main/java/com/budgetbuster/main.java
@@ -0,0 +1,15 @@
+package com.budgetbuster;
+
+
+
+
+public class main  {
+    public static void main (String[] args){
+        BankAccount bankAccount = new BankAccount("Joe","1234");
+        bankAccount.showMenu();
+
+    }
+}
+
+
+
